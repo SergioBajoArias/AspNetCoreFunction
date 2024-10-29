29ffbcc69272cb494625f37016c338c73bd73387
Sergio Bajo <sergio.bajo@xeridia.com>
2024-10-29 09:44:42 +0100

    Added support for RabbitMQ

--- MoviesFunction.cs
+++ MoviesFunction.cs
@@ -1,8 +1,11 @@
 using AspNetCoreFunction.RestApiClient;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace AspNetCoreFunction
 {
@@ -19,9 +22,31 @@ namespace AspNetCoreFunction
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            /*var client = new MoviesClient("https://app-test-webapi-westeurope-dev-001-e4grazbyh8gae3ff.westeurope-01.azurewebsites.net/", new HttpClient());
+            var movies = await client.MoviesAllAsync();
+            await client.MoviesPOSTAsync(new Movie())
+            return new OkObjectResult(movies);*/
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            const string queueName = "TEST";
+            var message = DateTime.Now.ToString();
+
+            channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            channel.BasicPublish(exchange: String.Empty, routingKey: queueName, basicProperties: null, body: Encoding.UTF8.GetBytes(message));
+
+            return new OkObjectResult($"Sent message {message} to queue {queueName}");
+        }
+
+        /*public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
             var client = new MoviesClient("https://app-test-webapi-westeurope-dev-001-e4grazbyh8gae3ff.westeurope-01.azurewebsites.net/", new HttpClient());
             var movies = await client.MoviesAllAsync();
+            await client.MoviesPOSTAsync(new Movie())
             return new OkObjectResult(movies);
-        }
+        }*/
     }
 }
